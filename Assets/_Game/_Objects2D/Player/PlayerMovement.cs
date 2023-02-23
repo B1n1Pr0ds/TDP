e887b2e6f89eea0a4e480e859bcd5ed83162b105
B1n1Pr0ds <123495359+B1n1Pr0ds@users.noreply.github.com>
2023-02-23 17:41:54 -0300

    Funny animation

--- Assets/_Game/_Objects2D/Player/PlayerMovement.cs
+++ Assets/_Game/_Objects2D/Player/PlayerMovement.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     private Animator playerAnimator;
     private Rigidbody2D rb;
     private float direction;
-    public float speed = 0.1f;
+    private float buttonDirection;
+    [SerializeField] private float speed = 0.1f;
     private Vector2 position;
-    private bool isWalking;
+    private Vector2 buttonPosition;
+    private bool walkingLeftButton;
+    private bool walkingRightButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +29,12 @@ public class PlayerMovement : MonoBehaviour
     {
 
         direction = Input.GetAxisRaw("Horizontal");
-      position = new Vector2(direction * speed, rb.velocity.y);
-        rb.AddForce(position, ForceMode2D.Impulse);
-        if(direction!=0)
+        position = new Vector2(direction * speed, rb.velocity.y);
+        Move(position);    
+    
+        if (direction != 0)
         {
+
             if (direction > 0)
                 gameObject.transform.localScale = new Vector2(2, 2);
 
@@ -35,10 +43,23 @@ public class PlayerMovement : MonoBehaviour
                 gameObject.transform.localScale = new Vector2(-2, 2);
             playerAnimator.SetBool("isWalking", true);
         }
-        if(direction == 0)
+        if (direction == 0)
         {
             playerAnimator.SetBool("isWalking", false);
         }
+    }
+    public void MoveRightButton()
+    {
+        walkingRightButton = true;
+    }
+    public void MoveLeftButton()
+    {
+        walkingLeftButton = true;
 
+    }
+
+    private void Move(Vector2 _position)
+    {
+        rb.AddForce(_position, ForceMode2D.Impulse);
     }
 }
