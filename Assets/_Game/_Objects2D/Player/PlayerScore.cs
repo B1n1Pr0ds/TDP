630e6973d58041aa5670846b9e055c5e18c46401
B1n1Pr0ds <123495359+B1n1Pr0ds@users.noreply.github.com>
2023-04-12 22:21:52 -0300

    a

--- Assets/_Game/_Objects2D/Player/PlayerScore.cs
+++ Assets/_Game/_Objects2D/Player/PlayerScore.cs
@@ -1,14 +1,25 @@
 
+using TMPro;
 using UnityEngine;
 
 
 public class PlayerScore : MonoBehaviour
 {
     //this script is responsable to make control of the player score and life
-    public int score =0;
+    
     private int life;
     private int startLife=10;
-    [SerializeField] GameObject gameOverScene;
+    [SerializeField] private  GameObject gameOverScene;
+    public TextMeshProUGUI timerText; 
+
+
+    [Header("PlayerScoreTracking")]
+        public int score = 0;
+        public int playerDeaths = 0;
+        public float playerPlayTime;
+    private bool shouldCountTime;
+
+
     public void AddScore(int _value)
     {
         score += _value;
@@ -22,11 +33,14 @@ public class PlayerScore : MonoBehaviour
         life = 0;
         gameObject.SetActive(false);
         gameOverScene.SetActive(true);
+        playerDeaths += 1;
+        shouldCountTime = false;
         
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.K)) 
+        timerText.text = CountTime();
+        if (Input.GetKeyDown(KeyCode.K)) 
         {
             TakeDamage(1000);
         }
@@ -38,5 +52,26 @@ public class PlayerScore : MonoBehaviour
     private void Start()
     {
         life = startLife;
+        gameOverScene.SetActive(false);
+        shouldCountTime = true;
+
+    }
+    public void EnableCountTime()
+    {
+        shouldCountTime = true;
+    }
+    public string CountTime()
+    {
+        if (shouldCountTime)
+        {
+            playerPlayTime += Time.deltaTime;
+        }
+
+        float _minutes = Mathf.FloorToInt(playerPlayTime / 60);
+        float _seconds = Mathf.FloorToInt(playerPlayTime % 60);
+        string _currentTime = string.Format("{00:00} : {01:00}", _minutes, _seconds);
+        return _currentTime;
+
+
     }
 }
--- Assets/_Game/_Shared/GameManager.cs
+++ Assets/_Game/_Shared/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private TextMeshProUGUI scoreTxt;
     [SerializeField] private TextMeshProUGUI gOScore;
+    [SerializeField] private TextMeshProUGUI playTime;
     private PlayerScore playerScoreScript;
 
   public void RestartLlevel()
@@ -26,7 +27,8 @@ public class GameManager : MonoBehaviour
     void ShowScore()
     {
        scoreTxt.text = "Score: " + playerScoreScript.score.ToString();
-        gOScore.text = "Your Final Score: " + playerScoreScript.score.ToString();
+       gOScore.text = "Your Score: " + playerScoreScript.score.ToString();
+       playTime.text = "Your time: " + playerScoreScript.CountTime();
     }
 }
     
